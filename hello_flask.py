1bfd276047a664d03dc15250dd2abc8b711db08a
joulongleu <joulongleu@yahoo.com.tw>
2021-07-11 06:49:53 +0800

    Add files via upload

---
+++ hello_flask.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello man'
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.run()
+    app.run(host='0.0.0.0', port=80, debug=True)
+#執行專案，程式會告知你連結網址
