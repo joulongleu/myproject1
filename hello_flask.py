186beae78fbfb02d242c1d546baacab814ef5d42
joulongleu <joulongleu@yahoo.com.tw>
2021-07-11 08:49:05 +0800

    test9test8

--- hello_flask.py
+++ hello_flask.py
@@ -7,5 +7,6 @@ def index():
 
 if __name__ == '__main__':
     app.debug = True
-    #app.run(bbbbbbbbbbb) 
-    app.run(host='0.0.0.0', port=80, debug=True)
+    #app.runaaaaaaa) 
+    #app.run(host='0.0.0.0', port=80, debug=True)
+    app.run()
