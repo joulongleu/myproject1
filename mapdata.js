fa9f322f84d911f5e7f492495c55655545993742
joulongleu <6103334+joulongleu@users.noreply.github.com>
2022-09-03 09:48:54 +0800

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,189 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "400", //'700' or 'responsive'
+    background_color: "#8b1f1f",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    TWN1156: {
+      name: "高雄市",
+      description: " "
+    },
+    TWN1158: {
+      name: "屏東縣",
+      description: " "
+    },
+    TWN1160: {
+      name: "台南市",
+      description: " "
+    },
+    TWN1161: {
+      name: "新竹市",
+      description: " "
+    },
+    TWN1162: {
+      name: "新竹縣",
+      description: " "
+    },
+    TWN1163: {
+      name: "宜蘭",
+      description: " "
+    },
+    TWN1164: {
+      name: "基隆市",
+      description: " "
+    },
+    TWN1165: {
+      name: "苗栗",
+      description: " "
+    },
+    TWN1166: {
+      name: "臺北市",
+      description: " "
+    },
+    TWN1167: {
+      name: "新北市",
+      description: " "
+    },
+    TWN1168: {
+      name: "桃園縣",
+      description: " "
+    },
+    TWN1169: {
+      name: "彰化",
+      description: " "
+    },
+    TWN1170: {
+      hover_color: "red",
+      description: "我的家鄉",
+      name: "嘉義",
+      url: "http://joulong.hopto.org:8333/"
+    },
+    TWN1171: {
+      name: "嘉義市",
+      description: " "
+    },
+    TWN1172: {
+      name: "花蓮",
+      description: " "
+    },
+    TWN1173: {
+      name: "南投縣",
+      description: " "
+    },
+    TWN1174: {
+      name: "台中市",
+      description: " "
+    },
+    TWN1176: {
+      hover_color: "red",
+      name: "雲林",
+      description: "我的家",
+      url: "http://joulong.hopto.org:8333/2022/08/02/%e6%99%ba%e6%85%a7%e5%b1%8b%e5%b1%95%e7%a4%ba/"
+    },
+    TWN1177: {
+      name: "台東",
+      description: " "
+    },
+    TWN3414: {
+      name: "澎湖",
+      description: " "
+    },
+    TWN3415: {
+      name: "金門",
+      description: " "
+    },
+    TWN5128: {
+      name: "連江",
+      description: " "
+    }
+  },
+  locations: {
+    "0": {
+      lat: "23.503490",
+      lng: "120.25243",
+      name: "老家",
+      description: "媽媽住家",
+      url: "http://joulong.hopto.org:8333"
+    },
+    "1": {
+      lat: "23.70564305830822",
+      lng: "120.53147274251366",
+      name: "我家",
+      url: "http://joulong.hopto.org:8333/2022/08/02/%e6%99%ba%e6%85%a7%e5%b1%8b%e5%b1%95%e7%a4%ba/",
+      description: "溫暖的家"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
